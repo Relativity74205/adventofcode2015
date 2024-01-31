622c7eca07feb47131ec60b44c836437fa3d226c
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2024-01-31 20:07:47 +0100

    feat: day07 finished, but WIP

--- src/main/kotlin/day07.kt
+++ src/main/kotlin/day07.kt
@@ -1,69 +1,63 @@
+import java.lang.Exception
 
-interface BitWiseOperation {
-    val operation: String
-    fun apply(): Int
-}
-
-
+// TODO create cache for a wire, if value is known, return it, otherwise calculate it
 private data class Wire(val identifier: String, val signalSource: String) {
+    private var signalValue: Int? = null
 
-    private fun getFoo(value: String, wires: Map<String, Wire>): Int {
+    private fun determineSignalValue(value: String, wires: Map<String, Wire>): Int {
         return if (value.toIntOrNull() != null) {
             value.toInt()
         } else {
-            wires[value]!!.getSignal(wires)
+            wires[value]!!.processSignalSource(wires)
         }
     }
 
-    fun getSignal(wires: Map<String, Wire>): Int {
+    fun setSignalValue(value: Int) {
+        signalValue = value
+    }
+
+    fun processSignalSource(wires: Map<String, Wire>): Int {
         val signalSourceParts = signalSource.split(" ")
+        if (signalValue != null) {
+            return signalValue!!
+        }
         when (signalSourceParts.size) {
             1 -> {
-                return getFoo(signalSourceParts[0], wires)
+                signalValue = determineSignalValue(signalSourceParts[0], wires)
             }
             2 -> {
-                val operation = signalSourceParts[0]
                 val signalSource = signalSourceParts[1]
-                return when (operation) {
-                    "NOT" -> {
-                        val signalSourceWire = wires[signalSource]!!
-                        Integer.parseInt(Integer.toBinaryString(signalSourceWire.getSignal(wires)).padStart(16, '0').replace('0', '2').replace('1', '0').replace('2', '1'), 2)
-                    }
-                    else -> {
-                        -1
-                    }
-                }
+                val signalSourceValue = wires[signalSource]!!.processSignalSource(wires)
+                signalValue = Integer.parseInt(Integer.toBinaryString(signalSourceValue).padStart(16, '0').replace('0', '2').replace('1', '0').replace('2', '1'), 2)
             }
             3 -> {
-                val operation = signalSourceParts[1]
-                when (operation) {
+                when (val operation = signalSourceParts[1]) {
                     "AND" -> {
-                        val signalSource1Wire = getFoo(signalSourceParts[0], wires)
-                        val signalSource2Wire = getFoo(signalSourceParts[2], wires)
-                        return signalSource1Wire.and(signalSource2Wire)
+                        val signalSource1Wire = determineSignalValue(signalSourceParts[0], wires)
+                        val signalSource2Wire = determineSignalValue(signalSourceParts[2], wires)
+                        signalValue = signalSource1Wire.and(signalSource2Wire)
                     }
                     "OR" -> {
-                        val signalSource1Wire = getFoo(signalSourceParts[0], wires)
-                        val signalSource2Wire = getFoo(signalSourceParts[2], wires)
-                        return signalSource1Wire.or(signalSource2Wire)
+                        val signalSource1Wire = determineSignalValue(signalSourceParts[0], wires)
+                        val signalSource2Wire = determineSignalValue(signalSourceParts[2], wires)
+                        signalValue = signalSource1Wire.or(signalSource2Wire)
                     }
                     "LSHIFT" -> {
-                        val signalSource = getFoo(signalSourceParts[0], wires)
+                        val signalSource = determineSignalValue(signalSourceParts[0], wires)
                         val amountBits = signalSourceParts[2]
-                        return signalSource.shl(amountBits.toInt())
+                        signalValue = signalSource.shl(amountBits.toInt())
                     }
                     "RSHIFT" -> {
-                        val signalSource = getFoo(signalSourceParts[0], wires)
+                        val signalSource = determineSignalValue(signalSourceParts[0], wires)
                         val amountBits = signalSourceParts[2]
-                        return signalSource.shr(amountBits.toInt())
+                        signalValue = signalSource.shr(amountBits.toInt())
                     }
-                    else -> {
-                        return -1
-                    }
+                    else -> { throw Exception("Unknown operation: $operation") }
                 }
             }
-            else -> { return -1 }
+            else -> { throw Exception("Unknown signal source: $signalSource") }
         }
+        return signalValue!!
     }
 }
 
@@ -73,25 +67,24 @@ private fun getWire(line: String): Wire {
     return Wire(identifier, signalSource)
 }
 
-private fun partA(lines: List<String>): Int {
-    val wires = lines.map { getWire(it) }
-    val wireMap = mutableMapOf<String, Wire>()
-    for (wire in wires) {
-        wireMap[wire.identifier] = wire
-    }
-    return wireMap["a"]!!.getSignal(wireMap.toMap())
+private fun partA(wireMap: Map<String, Wire>): Int {
+    val wireMapCopy = wireMap.map { it.key to it.value.copy() }.toMap()
+    return wireMapCopy["a"]!!.processSignalSource(wireMapCopy)
 }
 
-private fun partB(lines: List<String>): Int {
-    return 0
+private fun partB(wireMap: Map<String, Wire>): Int {
+    val wireMapCopy = wireMap.map { it.key to it.value.copy() }.toMap()
+    val override = wireMapCopy["a"]!!.processSignalSource(wireMapCopy)
+    val wireMapCopy2 = wireMap.map { it.key to it.value.copy() }.toMap().toMutableMap()
+    wireMapCopy2["b"] = Wire("b", override.toString())
+    return wireMapCopy2["a"]!!.processSignalSource(wireMapCopy2.toMap())
 }
 
 fun main() {
     val lines = readFile(7, false)
-    val linesDebug = readFile(7, true)
+    val wires = lines.map { getWire(it) }
+    val wireMap = wires.associateBy { it.identifier }
 
-//    println("Part A Debug: ${partA(linesDebug)}")
-    println("Part A: ${partA(lines)}")
-//    println("Part B Debug: ${partB(linesDebug)}")
-    println("Part B: ${partB(lines)}")
+    println("Part A: ${partA(wireMap)}")
+    println("Part B: ${partB(wireMap)}")
 }
